2b74a66079234a2239284878cd4c01b0d4bce5f7
Collin Busschots <collin.busschots4@gmail.com>
2024-10-31 12:57:10 +0100

    Test even elements

--- utils/searching_test.go
+++ utils/searching_test.go
@@ -13,3 +13,15 @@ func TestBinarySearchInts(t *testing.T) {
 		t.Fatalf("Index of element '%d' should be %d but was: %d\n", elementToFind, expectedIndex, index)
 	}
 }
+
+func TestBinarySearchIntsEven(t *testing.T) {
+	array := []int{1, 2, 3, 5, 7, 10, 12, 15}
+	elementToFind := 10
+
+	index := BinarySearchInts(array, elementToFind)
+
+	expectedIndex := 5
+	if index != expectedIndex {
+		t.Fatalf("Index of element '%d' should be %d but was: %d\n", elementToFind, expectedIndex, index)
+	}
+}
