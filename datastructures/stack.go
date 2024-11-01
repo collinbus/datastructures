ea694525f06cde89445dfc7da5a3bc2944a5f5a9
Collin Busschots <collin.busschots4@gmail.com>
2024-11-01 21:24:26 +0100

    Implement pop functionality

--- datastructures/stack.go
+++ datastructures/stack.go
@@ -2,6 +2,7 @@ package datastructures
 
 type Stack interface {
 	Push(element int)
+	Pop() int
 }
 
 type StaticStack struct {
@@ -17,4 +18,11 @@ func NewStaticStack(capacity int) *StaticStack {
 func (s *StaticStack) Push(element int) {
 	s.values[s.size] = element
 	s.size++
+}
+
+func (s *StaticStack) Pop() int {
+	s.size--
+	val := s.values[s.size]
+	s.values[s.size] = 0
+	return val
 }
--- datastructures/stack_test.go
+++ datastructures/stack_test.go
@@ -1,6 +1,9 @@
 package datastructures
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestPushOnStack(t *testing.T) {
 	capacity := 5
@@ -15,5 +18,29 @@ func TestPushOnStack(t *testing.T) {
 		if stack.values[i] != expectedValues[i] {
 			t.Fatalf("element at position %d is not matching\n", i)
 		}
+	}
+}
+
+func TestPopOnStack(t *testing.T) {
+	capacity := 5
+	stack := NewStaticStack(capacity)
+
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	poppedValue := stack.Pop()
+
+	
+	expectedPoppedValue := 3
+	expectedValues := []int{1,2,0,0,0}
+	
+	fmt.Println(stack.values)
+	for i := 0; i < capacity; i++ {
+		if stack.values[i] != expectedValues[i] {
+			t.Fatalf("element at position %d is not matching\n", i)
+		}
+	}
+	if expectedPoppedValue != poppedValue {
+		t.Fatalf("Expected popped value %d but got %d\n", expectedPoppedValue, poppedValue)
 	}
 }
